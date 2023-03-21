3345f2864cc77f54d5e3f00ca041bb9a65c25146
bitten2up <seant9140@gmail.com>
2023-03-20 21:05:17 -0500

    wafiojg

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,12 +2,14 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-use bootloader_api::{entry_point, BootInfo};
+use bootloader_api::{entry_point, BootInfo, FrameBufferInfo};
+mod framebuffer;
 
 entry_point!(kernel_main);
 
-fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+fn kernel_main(boot_info: &'static mut BootInfo, info: FrameBufferInfo) -> ! {
     if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        
         let mut value = 0x90;
         for byte in framebuffer.buffer_mut() {
             *byte = value;
